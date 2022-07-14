15e4d35e4fecfd10367b1c4338673cc68499a024
OH-front-end <oh.frontend@gmail.com>
2022-07-14 14:06:47 +0300

    add functionality for pause-play button

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,7 +1,11 @@
 const slides = document.querySelectorAll(".slide");
+const pausePlayButton = document.querySelector('#pause_play-btn');
+const prevButton = document.querySelector('#prev-btn');
+const nextButton = document.querySelector('#next-btn');
 const SLIDES_LENGTH = slides.length;
 let currentSlide = 0;
 let interval = setInterval(goToNth, 1000);
+let isPlay = true;
 
 function goToNth() {
   slides[currentSlide].classList.toggle('active');
@@ -9,3 +13,25 @@ function goToNth() {
   slides[currentSlide].classList.toggle('active');
 }
 
+function pause() {
+  clearInterval(interval);
+  pausePlayButton.innerHTML = 'Play';
+  isPlay = false;
+}
+
+function play() {
+  interval = setInterval(goToNth, 1000);
+  pausePlayButton.innerHTML = 'Pause';
+  isPlay = true;
+}
+
+function pausePlay() {
+  if (isPlay) {
+    pause();
+  } else {
+    play();
+  }
+}
+
+
+pausePlayButton.addEventListener('click', pausePlay);
